e9defad0ca9a3c49ffd2f4b597964af9cf5d350a
jonghyeon-paper <lieniece@gmail.com>
2019-09-04 22:09:58 +0900

    함수 분리

--- gradle-SpringbootWithHibernate/src/test/java/demo/CriteriaTest.java
+++ gradle-SpringbootWithHibernate/src/test/java/demo/CriteriaTest.java
@@ -3,9 +3,13 @@ package demo;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityManagerFactory;
@@ -90,6 +94,33 @@ public class CriteriaTest {
         return new Gson().fromJson(jsonString, type);
     }
     
+    public static final String EQUAL = "equal";
+    public static final String STARTING_WITH = "startingWith";
+    public static final String ENDING_WITH = "endingWith";
+    public static final String CONTAINING = "containing";
+    public static final String BETWEEN = "between";
+    public static final String LESS_THAN = "lessThan";
+    public static final String LESS_THAN_EQUAL = "lessThanEqual";
+    public static final String GREATER_THAN = "greaterThan";
+    public static final String GREATER_THAN_EQUAL = "greaterThanEqual";
+    public static final String IN = "in";
+    
+    @SuppressWarnings("serial")
+    public static final Set<String> ACTION_SET = new HashSet<String>() {
+        {
+            add(EQUAL);
+            add(STARTING_WITH);
+            add(ENDING_WITH);
+            add(CONTAINING);
+            add(BETWEEN);
+            add(LESS_THAN);
+            add(LESS_THAN_EQUAL);
+            add(GREATER_THAN);
+            add(GREATER_THAN_EQUAL);
+            add(IN);
+        }
+    };
+    
     private List<Customer> criteriaService(Map<String, Object> whereClauseMap) {
         SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
         Session session = sessionFactory.openSession();
@@ -99,93 +130,124 @@ public class CriteriaTest {
         Root<Customer> customer = criteriaQuery.from(Customer.class);
         
         
-        List<Predicate> predicateList = new ArrayList<>();
-        whereClauseMap.entrySet().stream()
-            .forEach(item -> {
-                String key = item.getKey();
-                if (key.split(":").length == 1) {
-                    predicateList.add(criteriaBuilder.equal(customer.get(key), item.getValue()));
-                } else {
-                    String columnName = key.split(":")[0];
-                    String action = key.split(":")[1];
-                    switch (action) {
-                    case "startingWith" :
-                        predicateList.add(criteriaBuilder.like(customer.get(columnName), item.getValue() + "%"));
-                        break;
-                    case "endingWith" :
-                        predicateList.add(criteriaBuilder.like(customer.get(columnName), "%" + item.getValue()));
-                        break;
-                    case "containing" :
-                        predicateList.add(criteriaBuilder.like(customer.get(columnName), "%" + item.getValue() + "%"));
-                        break;
-                    case "between" :
-                        if (!(item.getValue() instanceof ArrayList)) {
+//        List<Predicate> predicateList = new ArrayList<>();
+        List<Predicate> predicateList = whereClauseMap.entrySet().stream()
+                .map(conditionItem -> {
+                    String columnName = this.getColumnName(conditionItem.getKey());
+                    String action = this.getAction(conditionItem.getKey());
+                    
+                    if (EQUAL.equals(action)) {
+                        return criteriaBuilder.equal(customer.get(columnName), conditionItem.getValue());
+                    } else if (STARTING_WITH.equals(action)) {
+                        return criteriaBuilder.like(customer.get(columnName), conditionItem.getValue() + "%");
+                    }  else if (ENDING_WITH.equals(action)) {
+                        return criteriaBuilder.like(customer.get(columnName), "%" + conditionItem.getValue());
+                    } else if (CONTAINING.equals(action)) {
+                        return criteriaBuilder.like(customer.get(columnName), "%" + conditionItem.getValue() + "%");
+                    } else if (BETWEEN.equals(action)) {
+                        if (!(conditionItem.getValue() instanceof ArrayList)) {
                             throw new RuntimeException("'" + columnName + "' value invalid.");
                         }
-                        List<?> valueList = ((List<?>) item.getValue());
+                        List<?> valueList = ((List<?>) conditionItem.getValue());
                         if (valueList.size() != 2) {
                             throw new RuntimeException("'" + columnName + "' value length invalid.");
                         }
+                        
+                        String fore = String.valueOf(valueList.get(0));
+                        String rear = String.valueOf(valueList.get(1));
                         if (customer.get(columnName).getJavaType().equals(String.class)) {
-                            String fore = String.valueOf(valueList.get(0));
-                            String rear = String.valueOf(valueList.get(1));
-                            predicateList.add(criteriaBuilder.between(customer.get(columnName), fore, rear));
-                        } else if (customer.get(columnName).getJavaType().equals(BigDecimal.class)) {
-                            BigDecimal fore = new BigDecimal(String.valueOf(valueList.get(0)));
-                            BigDecimal rear = new BigDecimal(String.valueOf(valueList.get(1)));
-                            predicateList.add(criteriaBuilder.between(customer.get(columnName), fore, rear));
+                            return criteriaBuilder.between(customer.get(columnName), fore, rear);
+                        } else if (customer.get(columnName).getJavaType().equals(BigDecimal.class) ||
+                                Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
+                            return criteriaBuilder.between(customer.get(columnName), new BigDecimal(fore), new BigDecimal(rear));
                         } else if (customer.get(columnName).getJavaType().equals(Timestamp.class)) {
-                            Timestamp fore = new Timestamp(System.currentTimeMillis());
-                            Timestamp rear = new Timestamp(System.currentTimeMillis());
-                            predicateList.add(criteriaBuilder.between(customer.get(columnName), fore, rear));
-                        } else if (Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
-                            BigDecimal fore = new BigDecimal(String.valueOf(valueList.get(0)));
-                            BigDecimal rear = new BigDecimal(String.valueOf(valueList.get(1)));
-                            predicateList.add(criteriaBuilder.between(customer.get(columnName), fore, rear));
+                            return criteriaBuilder.between(customer.get(columnName), this.sringToTimestamp(fore), this.sringToTimestamp(rear));
                         }
-                        break;
-                    case "lessThan" :
+                    } else if (LESS_THAN.equals(action)) {
+                        String value = String.valueOf(conditionItem.getValue());
                         if (customer.get(columnName).getJavaType().equals(String.class)) {
-                            String value = String.valueOf(item.getValue());
-                            predicateList.add(criteriaBuilder.lessThan(customer.get(columnName), value));
-                        } else if (customer.get(columnName).getJavaType().equals(BigDecimal.class)) {
-                            BigDecimal value = new BigDecimal(String.valueOf(item.getValue()));
-                            predicateList.add(criteriaBuilder.lessThan(customer.get(columnName), value));
+                            return criteriaBuilder.lessThan(customer.get(columnName), value);
+                        } else if (customer.get(columnName).getJavaType().equals(BigDecimal.class) ||
+                                Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
+                            return criteriaBuilder.lessThan(customer.get(columnName), new BigDecimal(value));
                         } else if (customer.get(columnName).getJavaType().equals(Timestamp.class)) {
-                            Timestamp value = new Timestamp(System.currentTimeMillis());
-                            predicateList.add(criteriaBuilder.lessThan(customer.get(columnName), value));
-                        } else if (Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
-                            BigDecimal value = new BigDecimal(String.valueOf(item.getValue()));
-                            predicateList.add(criteriaBuilder.lessThan(customer.get(columnName), value));
+                            return criteriaBuilder.lessThan(customer.get(columnName), this.sringToTimestamp(value));
                         }
-                        break;
-                    case "lessThanEqual" :
-                        break;
-                    case "greaterThan" :
-                        break;
-                    case "greaterThanEqual" :
-                        break;
-                    case "in" :
-                        if (!(item.getValue() instanceof ArrayList)) {
+                    } else if (LESS_THAN_EQUAL.equals(action)) {
+                        String value = String.valueOf(conditionItem.getValue());
+                        if (customer.get(columnName).getJavaType().equals(String.class)) {
+                            return criteriaBuilder.lessThanOrEqualTo(customer.get(columnName), value);
+                        } else if (customer.get(columnName).getJavaType().equals(BigDecimal.class) ||
+                                Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
+                            return criteriaBuilder.lessThanOrEqualTo(customer.get(columnName), new BigDecimal(value));
+                        } else if (customer.get(columnName).getJavaType().equals(Timestamp.class)) {
+                            return criteriaBuilder.lessThanOrEqualTo(customer.get(columnName), this.sringToTimestamp(value));
+                        }
+                    } else if (GREATER_THAN.equals(action)) {
+                        String value = String.valueOf(conditionItem.getValue());
+                        if (customer.get(columnName).getJavaType().equals(String.class)) {
+                            return criteriaBuilder.greaterThan(customer.get(columnName), value);
+                        } else if (customer.get(columnName).getJavaType().equals(BigDecimal.class) ||
+                                Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
+                            return criteriaBuilder.greaterThan(customer.get(columnName), new BigDecimal(value));
+                        } else if (customer.get(columnName).getJavaType().equals(Timestamp.class)) {
+                            return criteriaBuilder.greaterThan(customer.get(columnName), this.sringToTimestamp(value));
+                        }
+                    } else if (GREATER_THAN_EQUAL.equals(action)) {
+                        String value = String.valueOf(conditionItem.getValue());
+                        if (customer.get(columnName).getJavaType().equals(String.class)) {
+                            return criteriaBuilder.greaterThanOrEqualTo(customer.get(columnName), value);
+                        } else if (customer.get(columnName).getJavaType().equals(BigDecimal.class) ||
+                                Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
+                            return criteriaBuilder.greaterThanOrEqualTo(customer.get(columnName), new BigDecimal(value));
+                        } else if (customer.get(columnName).getJavaType().equals(Timestamp.class)) {
+                            return criteriaBuilder.greaterThanOrEqualTo(customer.get(columnName), this.sringToTimestamp(value));
+                        }
+                    } else if (IN.equals(action)) {
+                        if (!(conditionItem.getValue() instanceof ArrayList)) {
                             throw new RuntimeException("'" + columnName + "' value invalid.");
                         }
                         if (Number.class.isAssignableFrom(customer.get(columnName).getJavaType())) {
-                            List<?> valueList2 = ((List<?>) item.getValue());
-                            predicateList.add(customer.get(columnName).in(valueList2.stream().map(item2 -> new BigDecimal(String.valueOf(item2))).collect(Collectors.toList())));
+                            List<?> valueList2 = ((List<?>) conditionItem.getValue());
+                            return customer.get(columnName).in(valueList2.stream().map(item2 -> new BigDecimal(String.valueOf(item2))).collect(Collectors.toList()));
                         } else {
-                            predicateList.add(customer.get(columnName).in(item.getValue()));
+                            return customer.get(columnName).in(conditionItem.getValue());
                         }
-                        break;
-                    default :
-                        throw new RuntimeException("'" + action + "' action is not supperted.");
                     }
-                }
-            });
+                    return null;
+                })
+                .collect(Collectors.toList());
+        
         criteriaQuery.select(customer).where(predicateList.toArray(new Predicate[predicateList.size()]));
         
         Query<Customer> query = session.createQuery(criteriaQuery);
         List<Customer> customerList = query.getResultList();
         session.close();
         return customerList;
+    }
+    
+    private String getColumnName(String key) {
+        return  key.split(":")[0];
+    }
+    
+    private String getAction(String key) {
+        String[] keyNameArray = key.split(":");
+        String action = keyNameArray[1];
+        if (keyNameArray.length == 1) {
+            return EQUAL;
+        }
+        if (!ACTION_SET.contains(action)) {
+            throw new RuntimeException("'" + action + "' is not support.");
+        }
+        return action;
+    }
+    
+    private Timestamp sringToTimestamp(String value) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return new Timestamp(sdf.parse(value).getTime());
+        } catch (ParseException e) {
+            throw new RuntimeException("Date-string invalid.");
+        }
     }
 }

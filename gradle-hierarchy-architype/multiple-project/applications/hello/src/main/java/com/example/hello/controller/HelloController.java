8361f5995b1ac67b2705cb696e396e710edd0a83
"_sCream" <"_sCream"@ONE9001342N001.onestorecorp.ext>
2022-10-19 17:36:46 +0900

    examples of including other projects

--- gradle-hierarchy-architype/multiple-project/applications/hello/src/main/java/com/example/hello/controller/HelloController.java
+++ gradle-hierarchy-architype/multiple-project/applications/hello/src/main/java/com/example/hello/controller/HelloController.java
@@ -1,13 +1,17 @@
-package com.example.hello;
+package com.example.hello.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.core.config.SomethingConfig;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
+        SomethingConfig somethingConfig = new SomethingConfig();
+        somethingConfig.initConfig();
         return "Greetings from Spring Boot!";
     }
 }

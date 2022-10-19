293d593673d4e3ec42db335cf689afb1f62e149c
"_sCream" <"_sCream"@ONE9001342N001.onestorecorp.ext>
2022-10-19 17:28:10 +0900

    add core project

---
+++ gradle-hierarchy-architype/multiple-project/modules/core/src/main/java/com/example/core/config/SomethingConfig.java
@@ -0,0 +1,8 @@
+package com.example.core.config;
+
+public class SomethingConfig {
+
+    public void initConfig() {
+        System.out.println("something configuration initializing...");
+    }
+}

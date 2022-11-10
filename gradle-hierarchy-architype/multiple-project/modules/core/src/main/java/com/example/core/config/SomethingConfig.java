30e68d73e7eb54b6cf1dac59fa923ac752bba365
_sCream <lieniece@gmail.com>
2022-11-10 15:11:39 +0900

    clean

--- gradle-hierarchy-architype/multiple-project/modules/core/src/main/java/com/example/core/config/SomethingConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.core.config;
-
-public class SomethingConfig {
-
-    public void initConfig() {
-        System.out.println("something configuration initializing...");
-    }
-}

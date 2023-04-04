ca16435a1503cb787ace8f2c3cfd5c529a83ca38
_sCream <lieniece@gmail.com>
2023-04-04 13:32:58 +0900

    security accept all config

--- gradle-hierarchy-architype/multiple-project/modules/security-token/src/main/java/com/example/modules/security/config/SecurityConfig.java
+++ gradle-hierarchy-architype/multiple-project/modules/security-token/src/main/java/com/example/modules/security/config/SecurityConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -22,28 +19,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-            )
-            .formLogin((form) -> form
-                .loginPage("/login")
-                .permitAll()
-            )
-            .logout((logout) -> logout.permitAll());
+        .cors()
+            .and()
+        .csrf()
+            .disable()
+        //.exceptionHandling()
+        //    .authenticationEntryPoint(...?)
+        //    .and()
+        .authorizeRequests()
+            .anyRequest().permitAll() // temporary accept all
+            .and()
+        .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
-    }
-    
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-             User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
